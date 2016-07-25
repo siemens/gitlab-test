2e361c2677f7ac50589d4695bf8874c2a38f5ec2
Sean McGivern <sean@gitlab.com>
2016-07-25 11:47:15 +0100

    Replace double quotes with single quotes

--- files/ruby/popen.rb
+++ files/ruby/popen.rb
@@ -6,13 +6,13 @@ module Popen
 
   def popen(cmd, path=nil)
     unless cmd.is_a?(Array)
-      raise RuntimeError, "System commands must be given as an array of strings"
+      raise RuntimeError, 'System commands must be given as an array of strings'
     end
 
     path ||= Dir.pwd
 
     vars = {
-      "PWD" => path
+      'PWD' => path
     }
 
     options = {
@@ -23,7 +23,7 @@ module Popen
       FileUtils.mkdir_p(path)
     end
 
-    @cmd_output = ""
+    @cmd_output = ''
     @cmd_status = 0
 
     Open3.popen3(vars, *cmd, options) do |stdin, stdout, stderr, wait_thr|
