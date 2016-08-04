1450cd639e0bc6721eb02800169e464f212cde06
Sean McGivern <sean@gitlab.com>
2016-08-04 18:24:25 +0100

    Single quotes and compact hashes

--- files/ruby/popen.rb
+++ files/ruby/popen.rb
@@ -6,24 +6,20 @@ module Popen
 
   def popen(cmd, path=nil)
     unless cmd.is_a?(Array)
-      raise RuntimeError, "System commands must be given as an array of strings"
+      raise RuntimeError, 'System commands must be given as an array of strings'
     end
 
     path ||= Dir.pwd
 
-    vars = {
-      "PWD" => path
-    }
+    vars = { 'PWD' => path }
 
-    options = {
-      chdir: path
-    }
+    options = { chdir: path }
 
     unless File.directory?(path)
       FileUtils.mkdir_p(path)
     end
 
-    @cmd_output = ""
+    @cmd_output = ''
     @cmd_status = 0
 
     Open3.popen3(vars, *cmd, options) do |stdin, stdout, stderr, wait_thr|
